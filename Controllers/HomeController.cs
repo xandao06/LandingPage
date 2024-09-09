be9f24dfb1ef86af4c7e6f3a446fe9a6ff43e541
Alexandre Altenhofen <alexandre@cprautomacao.com.br>
2024-09-09 08:10:55 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+using LandingPage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LandingPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+$(document).ready(function () {
+    $('#mobile_btn').on('click', function () {
+        $('#mobile_menu').toggleClass('active')
+        $('#mobile_btn').find('i').toggleClass('fa-x')
+    });
+});
